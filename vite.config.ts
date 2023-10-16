865504f4ad509e5521af3eff40295201f6058319
KenJiang228 <113347270+KenJiang228@users.noreply.github.com>
2023-10-16 15:43:36 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,13 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue", "v-calendar"],
+      external: [
+        "vue",
+        "v-calendar",
+        "vue-i18n",
+        "vue-final-modal",
+        "@phosphor-icons/vue",
+      ],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps

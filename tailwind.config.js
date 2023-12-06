865afb077742244c7d16c2e5b4a529b99496c0a2
KenJiang228 <113347270+KenJiang228@users.noreply.github.com>
2023-12-06 21:23:20 +0800

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ const animate = require("tailwindcss-animate");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx,vue}",
-    "./components/**/*.{ts,tsx,vue}",
-    "./app/**/*.{ts,tsx,vue}",
-    "./src/**/*.{ts,tsx,vue}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     container: {
       center: true,
